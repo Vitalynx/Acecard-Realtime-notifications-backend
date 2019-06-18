495febc3006f53f93f9cd7a78a9a7ca0e9eb0101
cmi <cmi@145.137.EARLY-REGISTRATION.of.SURFnet.invalid>
2019-06-18 17:00:15 +0200

    small change

--- sendnotif.js
+++ sendnotif.js
@@ -1,14 +1,40 @@
 const redis = require('redis');
 const client = redis.createClient({ host: '199.247.8.9'})
 
-const notification = {
-    type: "notification",
-    name: "deposit",
-    amount: Math.floor(Math.random() * 350),
-    date: new Date()
-}
+// const notification = {
+//     type: "notification",
+//     name: "deposit",
+//     amount: Math.floor(Math.random() * 350),
+//     updated_balance: Math.floor(Math.random() * 350),
+//     datetime: new Date()
+// }
 
 var uuid = "884e13f3-3ae1-4399-bad4-4495af7bdfda"
+
+const getFromRedis = () => {
+    client.lrange(uuid, 0, -1, (err, list) => {
+    //    if (list.length !== notifications.length) {
+    //       notifications = list.map(element => JSON.parse(element));
+    //       io.emit("event", notifications);
+    //       console.log("EMITTED EVENT");
+    //       console.log(notifications[0]);
+    //    }
+        var notifications = list.map(element => JSON.parse(element));
+
+        var oldbalance = notifications && notifications[0] && parseInt(notifications[0].updated_balance) - parseInt(notifications[0].amount)
+        var am = Math.floor(Math.random() * 100);
+        var notification = {
+            type: "notification",
+            name: "deposit",
+            amount: oldbalance,
+            updated_balance: oldbalance + am,
+            datetime: new Date()
+        }
+
+        sendEvent(uuid, notification);
+
+    })
+ }
 
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
@@ -26,4 +52,4 @@ const sendEvent = (uuid, event) => {
     })
 }
 
-sendEvent(uuid, notification);
+getFromRedis();
